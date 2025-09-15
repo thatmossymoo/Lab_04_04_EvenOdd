e3897b90627d7e65fba58b606b623420bb57fb50
thatmossymoo <grace.lafountain3@gmail.com>
2025-09-15 16:28:40 -0400

    FINAL COMMIT

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -1,9 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main
+public class EvenOdd
 {
     public static void main(String[] args)
     {
-
+    int numberToExamine = 2;
+    int evenodd = numberToExamine % 2;
+    System.out.println("Result of modulo operation (If 0, number is even, otherwise, number is odd): \n" + evenodd);
     }
 }
